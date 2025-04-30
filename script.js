e78607d0d78067666db21e86bf30a38c6b5199a0
aditya desai <addityadesai00034@gmail.com>
2025-04-30 22:47:41 +0530

    cursor conflict resolving

--- script.js
+++ script.js
@@ -45,3 +45,29 @@ function loadinganimation() {
     } )
 }
 loadinganimation()
+document.addEventListener("mousemove",function(dets){
+    gsap.to("#cursor",{
+        left:dets.x,
+        top:dets.y
+    })
+})
+document.querySelectorAll(".child").addEventListener ("mouseenter",function() {
+    gsap.to("#cursor", {
+        transform:'translate(-50% , -50%)  scale(1)'
+    })
+})
+document.querySelectorAll(".child").addEventListener ("mouseleave",function() {
+    gsap.to("#cursor", {
+        transform:'translate(-50% , -50%)  scale(0)'
+    })
+})
+
+var a = document.querySelectorAll(".child") 
+a.forEach(function(elem) {
+    elem.addEventListener("mouseenter",function() {
+        gsap.to("#cursor", {
+            transform:'translate(-50%,-50%) scale(1)'
+        })
+    })
+   
+})
